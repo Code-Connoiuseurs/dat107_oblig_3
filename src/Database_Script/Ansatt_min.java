da2a5009ddf77f0a25687617e99e586cd19d9e54
B676649 <Bendik_bb@hotmail.com>
2024-03-19 00:58:10 +0100

    Iterasjon 2
    Oppdatering av Iterasjon 2.

--- src/Database_Script/Ansatt_min.java
+++ src/Database_Script/Ansatt_min.java
@@ -9,23 +9,23 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "ansatt", schema = "oblig3_min")
 public class Ansatt_min {
-	
-	@Id private Integer ansatt_id;
-		private String brukernavn;
-		private String fornavn;
-		private String etternavn;
-		private LocalDate ansett_dato;
-		private String stilling;
-		private Double maanedslonn;
-		private String avdeling_id;
 
-	
+	@Id
+	private Integer ansatt_id;
+	private String brukernavn;
+	private String fornavn;
+	private String etternavn;
+	private LocalDate ansett_dato;
+	private String stilling;
+	private Double maanedslonn;
+	private String avdeling_id;
+
 	public Ansatt_min() {
 	}
-	
-	public Ansatt_min(String brukernavn, String fornavn, String etternavn, LocalDate ansett_dato,
-			String stilling,Double maanedslonn, String avdeling) {
-		
+
+	public Ansatt_min(String brukernavn, String fornavn, String etternavn, LocalDate ansett_dato, String stilling,
+			Double maanedslonn, String avdeling) {
+
 		this.brukernavn = brukernavn;
 		this.fornavn = fornavn;
 		this.etternavn = etternavn;
@@ -34,62 +34,79 @@ public class Ansatt_min {
 		this.maanedslonn = maanedslonn;
 		this.avdeling_id = avdeling;
 	}
+
 	public Integer getAnsattId() {
 		return ansatt_id;
 	}
+
 	public void setAnsattId(Integer ansattId) {
 		this.ansatt_id = ansattId;
 	}
+
 	public String getBrukernavn() {
 		return brukernavn;
 	}
+
 	public void setBrukernavn(String brukernavn) {
 		this.brukernavn = brukernavn;
 	}
+
 	public String getFornavn() {
 		return fornavn;
 	}
+
 	public void setFornavn(String fornavn) {
 		this.fornavn = fornavn;
 	}
+
 	public String getEtternavn() {
 		return etternavn;
 	}
+
 	public void setEtternavn(String etternavn) {
 		this.etternavn = etternavn;
 	}
+
 	public LocalDate getaAnsettDato() {
 		return ansett_dato;
 	}
+
 	public void setAnsettDato(LocalDate ansettDato) {
 		this.ansett_dato = ansettDato;
 	}
+
 	public String getStilling() {
 		return stilling;
 	}
+
 	public void setStilling(String stilling) {
 		this.stilling = stilling;
 	}
+
 	public double getMndLonn() {
 		return maanedslonn;
 	}
+
 	public void setMndLonn(double mndLonn) {
 		this.maanedslonn = mndLonn;
 	}
+
 	public String getAvdeling() {
 		return avdeling_id;
 	}
+
 	public void setAvdeling(String avdeling) {
 		this.avdeling_id = avdeling;
 	}
+
 	public void skrivUt() {
 		System.out.println("Ansatt ID: " + ansatt_id);
-        System.out.println("Brukernavn: " + brukernavn);
-        System.out.println("Fornavn: " + fornavn);
-        System.out.println("Etternavn: " + etternavn);
-        System.out.println("Ansettelsesdato: " + ansett_dato);
-        System.out.println("Stilling: " + stilling);
-        System.out.println("Månedslønn: " + maanedslonn);
-        System.out.println("Avdeling: " + avdeling_id);
+		System.out.println("Brukernavn: " + brukernavn);
+		System.out.println("Fornavn: " + fornavn);
+		System.out.println("Etternavn: " + etternavn);
+		System.out.println("Ansettelsesdato: " + ansett_dato);
+		System.out.println("Stilling: " + stilling);
+		System.out.println("Månedslønn: " + maanedslonn);
+		System.out.println("Avdeling: " + avdeling_id);
 	}
 }
--- src/Database_Script/Main_min.java
+++ src/Database_Script/Main_min.java
@@ -1,56 +1,77 @@
 package Database_Script;
 
+import java.util.List;
 import java.util.Scanner;
 
-public class Main_min {    
-  
+public class Main_min {
+
 	private static final AnsattDAO_min ansattDAO_min = new AnsattDAO_min();
 	private static final Scanner scanner = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("q : stop programmet. \n"
-				+ "1 : Finn ansatt ved bruk av id. \n"
-				+ "2 : Finn ansatt ved bruk av brukernavn. \n"
-				+ "3 : Liste av alle anstte. \n"
-				+ "4 : Oppdater en ansatt sin stilling. \n"
-				+ "5 : Oppdater en ansatt sin lønn. \n"
+
+		System.out.println("q : stop programmet. \n" + "1 : Finn ansatt ved bruk av id. \n"
+				+ "2 : Finn ansatt ved bruk av brukernavn. \n" + "3 : Liste av alle anstte. \n"
+				+ "4 : Oppdater en ansatt sin stilling. \n" + "5 : Oppdater en ansatt sin lønn. \n"
 				+ "6 : legg inn en ny ansatt.");
-		
+
 		String valgt = scanner.nextLine();
 		try {
-		
-		switch (valgt) {
-		case "q":
-			scanner.close();
-			System.out.println("Programmet har stoppet");
-			break;
-		
-		case "1":
-			System.out.println("Ansatt id: ");
-			int ansattId = Integer.parseInt(scanner.nextLine());
-			Ansatt_min ansatt = ansattDAO_min.finnAnsattMedId(ansattId);
-			if(ansatt != null) {
-				ansatt.skrivUt();
-			} else {
-				System.out.println("Ansatt med Id: " + ansattId + " finnes ikke");
+
+			switch (valgt) {
+			case "q":
+				scanner.close();
+				System.out.println("Programmet har stoppet");
+				break;
+
+			case "1":
+				System.out.println("Ansatt id: ");
+				int ansattId = Integer.parseInt(scanner.nextLine());
+				Ansatt_min ansatt = ansattDAO_min.finnAnsattMedId(ansattId);
+				if (ansatt != null) {
+					ansatt.skrivUt();
+				} else {
+					System.out.println("Ansatt med Id: " + ansattId + " finnes ikke");
+				}
+				break;
+			case "2":
+				System.out.println("Ansatt brukernavn / initialer: ");
+				Ansatt_min ansattbruker = ansattDAO_min.finnAnsattMedInitial(scanner.nextLine());
+				if (ansattbruker != null) {
+					ansattbruker.skrivUt();
+				} else {
+					System.out.println("Ansatt med brukernavn eller initialer: " + ansattbruker + " ble ikke funnet");
+				}
+				break;
+
+			case "3":
+				System.out.println("Liste av ansatte:  \n");
+				List<Ansatt_min> ansatteListe = ansattDAO_min.listeAvAlleAnsatte();
+
+				if (ansatteListe != null) {
+					for(Ansatt_min a : ansatteListe) {
+					a.skrivUt();
+					System.out.println();
+					}
+				}
+				break;
+				
+			case "4":
+				System.out.println("Ansatt id: ");
+				
+				int idAnsatt = Integer.parseInt(scanner.nextLine());
+				System.out.println("Ny stilling: ");
+				
+				String nyStilling = scanner.nextLine();
+				System.out.println(ansattDAO_min.OppdaterStilling(idAnsatt, nyStilling));
+				break;
+				
+			case "5":
+				break;
 			}
-			break;
-		case "2":
-			System.out.println("Ansatt brukernavn / initialer: ");
-			Ansatt_min ansattbruker = ansattDAO_min.finnAnsattMedInitial(scanner.nextLine());
-			if(ansattbruker != null) {
-				ansattbruker.skrivUt();
-			} else {
-				System.out.println("Ansatt med brukernavn eller initialer: " + ansattbruker + " ble ikke funnet");
-			}
-			break;
-			}
+			
 		} catch (NumberFormatException e) {
 			System.out.println("Prøv igjen.");
 		}
 	}
-	
 }
-	
-
